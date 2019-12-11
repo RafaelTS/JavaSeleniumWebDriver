b3b4cbc6b6472574a2baeb9042209ca2b2c3fba7
RafaelTS <rafaeltorress@gmail.com>
2019-12-10 21:46:32 -0300

    first commit

--- src/main/java/FirstSteps.java
+++ src/main/java/FirstSteps.java
@@ -1,16 +1,84 @@
+import java.io.File;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.WebDriver;	
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;	
 
 
 public class FirstSteps {
 	
+	
 	@Test
-	public void Test() {
+	public void OutroTeste() {
+		
 		WebDriver driver = new FirefoxDriver();
-		driver.get("www.google.com");
-		Assert.assertEquals("Google", driver.getTitle());
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Teste de Escrita");
+		Assert.assertEquals("Teste de Escrita", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
+		driver.quit();
 	}
+	
+	@Test
+	public void mustInteractWithTextArea(){
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Teste\nquer dizer nova linha");
+		Assert.assertEquals("Teste\nquer dizer nova linha", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+		
+		driver.quit();
+		
+	}
+	
+	@Test
+	public void mustInteractWithRadioButton() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:sexo:0")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:sexo:0")).isSelected());
+		
+		driver.quit();
+				
+	}
+	
+	@Test
+	public void mustInteractWithChekbox() {
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:comidaFavorita:2")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:2")).isSelected());
+		driver.quit();
+		
+	}
+	
+	@Test
+	public void mustInteractWithCombo() {
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		WebElement element = driver.findElement(By.id("elementosFom:escolaridade"));
+		Select combo = new Select(element);
+		//this line is for example
+		//combo.selectByIndex(3);
+		combo.selectByValue("superior");
+		//this line is for example
+		//combo.deselectByVisibleText("mestrado");
+		Assert.assertEquals("superior", combo.getFirstSelectedOption().getText());
+		driver.quit();
+	}
+	
+	
+	
+	
 
 }
