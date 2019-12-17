d50746510b5229fbfabf179101683ccac35b44dc
RafaelTS <rafaeltorress@gmail.com>
2019-12-16 21:32:06 -0300

    aula 1612

--- src/main/java/FirstSteps.java
+++ src/main/java/FirstSteps.java
@@ -1,9 +1,13 @@
-import java.io.File;
+
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.internal.WebElementToJsonConverter;
 import org.openqa.selenium.support.ui.Select;
+
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;	
@@ -77,8 +81,52 @@ public class FirstSteps {
 		driver.quit();
 	}
 	
+	@Test
+	public void mustVerfyValuesCombo() {
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		WebElement element = driver.findElement(By.id("elementosFom:escolaridade"));
+		Select combo = new Select(element);
+		List<WebElement> options = combo.getOptions();
+		Assert.assertEquals(8, options.size());
+		
+		boolean encontrou = false;
+		for(WebElement option: options){
+			
+			if(option.getText().equals("Mestrado")) {
+				encontrou = true;
+				break;
+			}
+		
+		}
+		Assert.assertTrue(encontrou);
+	}
+	@Test
+	public void mustInteractWithMultiplusCombos() {
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		WebElement element = driver.findElement(By.id("elementosFom:esportes"));
+		Select combo = new Select(element);
+		combo.selectByVisibleText("Natacao");
+		combo.selectByVisibleText("Corrida");
+		combo.selectByVisibleText("O que eh esporte?");
+		
+		List<WebElement> allSelectedOptions = combo.getAllSelectedOptions();
+		Assert.assertEquals(3, allSelectedOptions.size());
+		
+		combo.deselectByVisibleText("Corrida");
+		
+		allSelectedOptions = combo.getAllSelectedOptions();
+		Assert.assertEquals(2, allSelectedOptions.size());
+		driver.quit();
+		
+		
+	}
 	
 	
 	
-
 }
